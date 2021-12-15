9a96995a9750f02ed6868aa174622d149a043236
Aadam Ali <ali.aadam@outlook.com>
2021-12-15 15:00:42 +0000

    create DEFAULT_CAPACITY

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,7 @@
 require_relative "bike"
 class DockingStation
+  DEFAULT_CAPACITY = 20
+
   def initialize
     @bikes = []
   end
@@ -17,7 +19,7 @@ class DockingStation
   private 
 
   def full?
-    @bikes.count >= 20
+    @bikes.count >= DEFAULT_CAPACITY
   end 
 
   def empty?
