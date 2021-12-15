3aecdc7b6ca759cff28b673e9cc1bfc1bc3c4557
Aadam Ali <ali.aadam@outlook.com>
2021-12-15 15:05:24 +0000

    updated test to include DEFAULT_CAPACITY

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -23,7 +23,7 @@ describe DockingStation do
   end 
 
   it "raises an error when the docking station is full" do 
-    20.times { subject.dock(Bike.new) }
+    DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
     expect { subject.dock(Bike.new) }.to raise_error "Station full"
   end 
 
