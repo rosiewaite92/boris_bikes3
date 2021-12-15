064a09dd34b1b411e3672c4d367d391899789d12
Aadam Ali <ali.aadam@outlook.com>
2021-12-15 15:38:02 +0000

    added test for reporting broken bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,9 @@
 require "bike"
 describe Bike do
   it {should respond_to(:working?)}
+
+  it 'a bike should be reported broken' do
+    subject.report_broken
+    expect(subject).not_to be_working
+  end
 end 
