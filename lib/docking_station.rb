779f1cc14be19904eda3701984e9fec54cfce305
Rosie Waite <rosiewaite92@gmail.com>
2021-12-15 14:55:42 +0000

    added private full? method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,13 +5,19 @@ class DockingStation
   end
 
   def dock(bike)
-    fail "Station full" if @bikes.count >= 20
+    fail "Station full" if full?
     @bikes << bike 
   end 
 
   def release_bike
     fail 'No bikes available' if @bikes.empty?
     @bikes.pop
+  end 
+
+  private 
+
+  def full?
+    @bikes.count >= 20
   end 
   
 end 
