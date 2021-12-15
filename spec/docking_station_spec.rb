e46cf09adeb3d2ec5ebd690ea3f48a2fc1f4608a
Rosie Waite <rosiewaite92@gmail.com>
2021-12-15 14:21:43 +0000

    station full test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,16 +5,24 @@ describe DockingStation do
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end 
   end 
+
   it 'releases a bike' do 
     bike = Bike.new
     subject.dock(bike)
     expect(subject.release_bike).to eq bike 
   end 
+
 it {should respond_to(:release_bike)}
   it {should respond_to(:dock).with(1).argument}
   it 'releases working bikes' do 
     bike = Bike.new
     expect(subject.dock(bike)).to be_working
   end 
+it "raises an error when the docking station is full" do 
+  bike = Bike.new
+  subject.dock(bike)
+  expect {subject.dock(Bike.new)}.to raise_error "Station full"
+end 
+
 end 
 
